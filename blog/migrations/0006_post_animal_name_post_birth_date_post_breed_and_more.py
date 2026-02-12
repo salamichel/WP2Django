4f98cd5d81b4e210afcc1f3e7bc1d28119547d49
Stéphane Alamichel <3256040+salamichel@users.noreply.github.com>
2026-02-12 13:38:40 +0100

    Add migration 0006 for post model changes

--- blog/migrations/0006_post_animal_name_post_birth_date_post_breed_and_more.py
+++ blog/migrations/0006_post_animal_name_post_birth_date_post_breed_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("blog", "0004_add_menuitem_linked_fk"),
+        ("blog", "0005_postgalleryimage"),
     ]
 
     operations = [
